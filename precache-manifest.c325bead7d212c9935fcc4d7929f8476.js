8f839d38a2c3714fdb7092ebe22ce698fb572d03
Viral Haria <viralhar@gmail.com>
2020-05-18 19:06:43 +0530

    Updates

---
+++ precache-manifest.c325bead7d212c9935fcc4d7929f8476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353f8fdc64d62555f4b11ea7e4740bed",
+    "url": "/react-baby-names/index.html"
+  },
+  {
+    "revision": "512bca970729804a1e65",
+    "url": "/react-baby-names/static/css/main.814ef9c3.chunk.css"
+  },
+  {
+    "revision": "7022cd7d3531f74649d6",
+    "url": "/react-baby-names/static/js/2.c622b61c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-baby-names/static/js/2.c622b61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512bca970729804a1e65",
+    "url": "/react-baby-names/static/js/main.cd758d54.chunk.js"
+  },
+  {
+    "revision": "1aab4382a5fa64e8bfc7",
+    "url": "/react-baby-names/static/js/runtime-main.01ad5b96.js"
+  }
+]);
